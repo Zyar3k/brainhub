d5905d57760bdf027e85904c47f3a6ad5eb45190
Arkadiusz Zygan <a.zygan@gmail.com>
2022-09-20 16:11:16 +0200

    Connect to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,19 +1,26 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 const PORT = 8000;
 
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6tpkm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Server init");
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const start = async () => {
   try {
     app.listen(PORT, () => {
+      const db = mongoose.connection;
+      db.on("error", (error) => console.error(error));
+      db.once("open", () => console.log("Connected to database"));
       console.log(`Server is listening on http://localhost:${PORT}`);
     });
   } catch (error) {
