71049cf70de46b632c86778c5fd783e6ff4e974b
Arkadiusz Zygan <a.zygan@gmail.com>
2022-09-20 20:39:52 +0200

    First lis render

--- client/src/components/BookingList/BookingList.jsx
+++ client/src/components/BookingList/BookingList.jsx
@@ -2,10 +2,35 @@ import { useContext } from "react";
 
 import { GlobalContext } from "../../context/GlobalProvider";
 
+import "./BookingList.scss";
+
 const BookingList = () => {
   const { bookings } = useContext(GlobalContext);
   console.log(bookings);
-  return <div>BookingList</div>;
+  return (
+    <section>
+      <h3>Our appointments</h3>
+      <div className="bookings">
+        {bookings.length === 0 ? (
+          "Loading..."
+        ) : (
+          <>
+            {bookings.map((book, index) => (
+              <div className="booking">
+                <p className="number">{index + 1}</p>
+                <p className="name">
+                  {book.name} {book.lastName}
+                </p>
+                <p className="email">{book.email}</p>
+                <p className="date">{book.bookingDate}</p>
+                <p className="actions"></p>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default BookingList;
